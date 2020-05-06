a87365597a5575fa3aed47c832fa0f24760e0a3e
kailanguage <41792700+kailanguage@users.noreply.github.com>
2020-05-06 22:41:08 +0800

    add automatically generated files by MyBatis

---
+++ src/test/java/com/kailang/engasst/engasst/EngAsstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kailang.engasst.engasst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngAsstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
